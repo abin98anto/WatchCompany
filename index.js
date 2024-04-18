5bbaec18d80710599ed05659612f2c3fe9de1fef
abin98anto <abin98anto@gmail.com>
2024-04-18 21:51:10 +0530

    404 done

--- index.js
+++ index.js
@@ -28,12 +28,32 @@ app.set("view engine", "ejs");
 app.set("views", [
   path.join(__dirname, "views/users"),
   path.join(__dirname, "views/admin"),
+  path.join(__dirname, "views/partials"),
 ]);
+// app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", user_route);
 
 app.use("/admin", admin_route);
+
+// Middleware to handle undefined routes
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.log(req.url.split("/")[1]);
+  let url = "";
+  let folder = "";
+  req.url.split("/")[1] == "admin" ? (folder = "admin") : (folder = "user");
+  req.url.split("/")[1] == "admin" ? (url = "/admin") : (url = "/");
+  res.status(err.status || 500);
+  res.render("404", { error: err, url: url, folder: folder });
+});
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
