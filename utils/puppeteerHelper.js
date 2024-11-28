7cf4ec605f6895b4c43ca0847eaf70e968ece7c3
abin98anto <abin98anto@gmail.com>
2024-11-28 14:55:15 +0530

    Fixed Invoice Download

--- utils/puppeteerHelper.js
+++ utils/puppeteerHelper.js
@@ -6,15 +6,13 @@ const launchBrowser = async () => {
     process.env.RENDER || process.env.AWS_LAMBDA_FUNCTION_NAME;
 
   if (isRenderEnv) {
-    // Configuration for Render or AWS Lambda
     return chromium.puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
+      executablePath: "/opt/render/.cache/puppeteer/chrome-linux/chrome",
       headless: chromium.headless,
     });
   } else {
-    // Configuration for local development
     return puppeteer.launch({
       headless: true,
     });
